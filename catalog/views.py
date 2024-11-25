42cadf0a9bc05efcfd041a708b98ea28ab028e35
Natasha Krasheninnikova <nk110905@mail.ru>
2024-11-25 16:17:27 +0700

    home web commit

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
-# Create your views here.
+
+def index(request):
+    done_requests = Request.objects.filter(status='В')[:4]
+    accepted_request_counter = Request.objects.filter(status='П').count()
+    return render(request, 'index.html', {
+        'done_requests': done_requests, 'accepted_request_counter': accepted_request_counter}
+    )
